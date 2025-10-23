2fe9c8339f204394b3fd31c655a1c572824c9e7b
Juan C Robles <github@juancrobles.com>
2025-10-23 12:09:39 -0600

    Update package name

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "nlohmann-json",
+    name: "json",
      platforms: [
         .iOS(.v12), .macOS(.v10_13), .tvOS(.v12), .watchOS(.v4), .visionOS(.v1)
     ],
