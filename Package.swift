844bf4e5b238059c2d5f2f2cdb7564194cc3f6ea
Juan C Robles <github@juancrobles.com>
2025-10-23 10:38:30 -0600

    Updated swift package definition to include the json-xcframework

--- Package.swift
+++ Package.swift
@@ -9,14 +9,20 @@ let package = Package(
         .iOS(.v12), .macOS(.v10_13), .tvOS(.v12), .watchOS(.v4), .visionOS(.v1)
     ],
     products: [
-        .library(name: "json", targets: ["json"])
+        .library(name: "json", targets: ["json"]),
+        .library(name: "json-framework", targets: ["jsonFramework"]),
     ],
     targets: [
         .target(
             name: "json",
             path: "single_include/nlohmann",
             publicHeadersPath: "."
-        )
+        ),
+        .binaryTarget(
+            name: "jsonFramework",
+            url: "https://github.com/JuanCRoblesDev/json/releases/download/v3.12.1/json.xcframework.zip",
+            checksum: "8a67fc9818e93d45116d1082092e9d8ee4db1cdd6cac6b7be271d65440ad625b"
+        ),
     ],
     cxxLanguageStandard: .cxx11
 )
